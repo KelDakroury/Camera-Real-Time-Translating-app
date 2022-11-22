5a3357ab0c6dbe9a4ec0a84b5a97160283d45cae
bl7awy <mahadelr19@gmail.com>
2022-11-22 14:45:08 +0300

    Add real-time object detection with yolov5

---
+++ main.py
@@ -0,0 +1,60 @@
+import time
+
+import cv2
+import yolov5
+
+def get_yolov5_model(size='n'):
+    model_name = f'yolov5{size}.pt'
+    model = yolov5.load(model_name)
+    # set model parameters
+    model.conf = 0.25  # NMS confidence threshold
+    model.iou = 0.45  # NMS IoU threshold
+    model.agnostic = False  # NMS class-agnostic
+    model.multi_label = True  # NMS multiple labels per box
+    model.max_det = 5  # maximum number of detections per image
+    model._get_name
+    return model
+
+
+def yolov5_process(model, frame):
+    results = model(frame)
+    new_frame = results.render()[0]
+    return new_frame
+
+
+
+if __name__ == '__main__':
+    # load pretrained model
+    model = get_yolov5_model(size='n')
+
+
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Can't open source")
+        exit()
+
+    w, h = (1280, 720)
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    # writer = cv2.VideoWriter('output.mp4', fourcc, 30, (w, h))
+
+    # Read until video is completed
+    while cap.isOpened():
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        old_time = time.time()
+        frame = yolov5_process(model, frame)
+        print(type(frame))
+        new_time = time.time()
+
+        cv2.imshow('Frame', frame)
+
+        # writer.write(frame)
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    # writer.release()
